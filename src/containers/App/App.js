661886f85b46a8e6a010ccc8ef0f5dea267da6fe
Firat Tale <talefirat@gmail.com>
2019-01-31 16:05:14 +0300

    Creating Card and CardList components

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,56 +2,13 @@ import React, { Component } from "react";
 import "./App.css";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import CardList from "../CardList/CardList";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <p>COntent</p>
-        <p>COntent</p>
-        <p>COntent</p>
-        <p>COntent</p>
-        <p>COntent</p>
-        <p>COntent</p>
-        <p>COntent</p>
-        <p>COntent</p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque tenetur
-        molestiae quaerat, enim dolor itaque distinctio, totam, obcaecati
-        explicabo optio tempora perspiciatis sunt sit ad unde sint doloribus
-        aspernatur dolore!Lorem Lorem ipsum, dolor sit amet consectetur
-        adipisicing elit. Rerum quia aperiam perferendis iusto fugit similique
-        provident at quaerat, nihil doloremque dicta voluptas placeat, earum
-        eaque quod facilis recusandae ab qui!
-        <p>COntent</p>
-        <p>COntent</p>
-        <p>COntent</p>
-        <p>COntent</p>
-        <p>COntent</p>
-        <p>COntent</p>
-        <p>COntent</p>
-        <p>COntent</p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque tenetur
-        molestiae quaerat, enim dolor itaque distinctio, totam, obcaecati
-        explicabo optio tempora perspiciatis sunt sit ad unde sint doloribus
-        aspernatur dolore!Lorem Lorem ipsum, dolor sit amet consectetur
-        adipisicing elit. Rerum quia aperiam perferendis iusto fugit similique
-        provident at quaerat, nihil doloremque dicta voluptas placeat, earum
-        eaque quod facilis recusandae ab qui!
-        <p>COntent</p>
-        <p>COntent</p>
-        <p>COntent</p>
-        <p>COntent</p>
-        <p>COntent</p>
-        <p>COntent</p>
-        <p>COntent</p>
-        <p>COntent</p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque tenetur
-        molestiae quaerat, enim dolor itaque distinctio, totam, obcaecati
-        explicabo optio tempora perspiciatis sunt sit ad unde sint doloribus
-        aspernatur dolore!Lorem Lorem ipsum, dolor sit amet consectetur
-        adipisicing elit. Rerum quia aperiam perferendis iusto fugit similique
-        provident at quaerat, nihil doloremque dicta voluptas placeat, earum
-        eaque quod facilis recusandae ab qui!
+        <CardList />
         <Footer />
       </div>
     );
---
+++ src/containers/CardList/CardList.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Card from "../../components/Card/Card";
+
+class CardList extends Component {
+  state = {};
+  render() {
+    return <Card />;
+  }
+}
+
+export default CardList;
