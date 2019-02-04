f7b1e498c327ea36d441a87c52a993c4059bcf58
Firat Tale <talefirat@gmail.com>
2019-02-04 16:11:31 +0300

    Updates

---
+++ precache-manifest.17e9047e5e999589d6a8766d4f21f2d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc02b5f05998eae1ba4a",
+    "url": "/HackerNews-Top500/static/css/main.d4192ad7.chunk.css"
+  },
+  {
+    "revision": "cc02b5f05998eae1ba4a",
+    "url": "/HackerNews-Top500/static/js/main.cc02b5f0.chunk.js"
+  },
+  {
+    "revision": "74a907858bcd41241824",
+    "url": "/HackerNews-Top500/static/js/1.74a90785.chunk.js"
+  },
+  {
+    "revision": "76e5e486d6abf69833e2",
+    "url": "/HackerNews-Top500/static/js/runtime~main.76e5e486.js"
+  },
+  {
+    "revision": "4a5550189e6d1e98bdfd94aa2d1b4d55",
+    "url": "/HackerNews-Top500/index.html"
+  }
+];
