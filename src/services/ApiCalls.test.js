fccee1cece9187c76b3c6e2ae707756f2d6347b3
Firat Tale <talefirat@gmail.com>
2019-02-06 12:11:10 +0300

    reafactor apicalls function

--- src/services/ApiCalls.test.js
+++ src/services/ApiCalls.test.js
@@ -13,13 +13,13 @@ it("should fetch ids when you call fetchIds", done => {
   fetchIds("www.google.com");
   expect(global.fetch).toHaveBeenCalledTimes(1);
   expect(global.fetch).toHaveBeenCalledWith("www.google.com");
-  global.fetch.mockClear(); // 7
-  done(); // 8
+  global.fetch.mockClear();
+  done();
 });
 
 it("should fetch infos when you call fetchArticleInfos", done => {
   fetchArticleInfos([1, 2, 3]);
   expect(global.fetch).toHaveBeenCalledTimes(3);
-  global.fetch.mockClear(); // 7
-  done(); // 8
+  global.fetch.mockClear();
+  done();
 });
--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -1,27 +1,37 @@
 export const fetchIds = async url => {
-  const response = await fetch(url);
-  const ids = response.json();
-  return ids;
+  try {
+    const response = await fetch(url);
+    const ids = response.json();
+    return ids;
+  } catch (error) {
+    console.error("error", error);
+  }
 };
 
 export const fetchArticleInfos = async articleIds => {
-  const result = {
-    score: [],
-    title: [],
-    author: [],
-    url: []
-  };
+  try {
+    const result = {
+      score: [],
+      title: [],
+      author: [],
+      url: []
+    };
 
-  let requests = articleIds.map(id =>
-    fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`)
-  );
-  const rez = await Promise.all(requests);
-  const data = await Promise.all(rez.map(res => res.json()));
-  data.forEach((item, i) => {
-    result.score.push(data[i].score);
-    result.title.push(data[i].title);
-    result.author.push(data[i].by);
-    result.url.push(data[i].url);
-  });
-  return result;
+    let requests = await articleIds.map(id =>
+      fetch(
+        `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
+      )
+    );
+    const rez = await Promise.all(requests);
+    const data = await Promise.all(rez.map(res => res.json()));
+    data.forEach((item, i) => {
+      result.score.push(data[i].score);
+      result.title.push(data[i].title);
+      result.author.push(data[i].by);
+      result.url.push(data[i].url);
+    });
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
 };
